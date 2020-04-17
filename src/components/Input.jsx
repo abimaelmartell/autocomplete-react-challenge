0f9a8d43d40850f8a9330c4ad087c3c8efbb7641
Abimael Martell <abimex@gmail.com>
2020-04-16 18:30:26 -0700

    Add comment on query escape

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,6 +4,7 @@ class Input extends Component {
   handleChange(event) {
     const value = event.target.value;
 
+    // filter all non word characters
     const escaped = value.replace(/[\W_]+/g,"");
 
     this.props.onChange(escaped);
